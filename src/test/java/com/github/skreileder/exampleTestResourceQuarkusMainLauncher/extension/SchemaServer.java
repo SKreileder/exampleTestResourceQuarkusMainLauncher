5bcbcf2d1659fe454d1570b72a59315dbb92eaa5
Stefan Kreileder <s.kreileder@adito.de>
2025-01-28 07:27:23 +0100

    fix: forgott to commit SchemaServer.java changes

--- src/test/java/com/github/skreileder/exampleTestResourceQuarkusMainLauncher/extension/SchemaServer.java
+++ src/test/java/com/github/skreileder/exampleTestResourceQuarkusMainLauncher/extension/SchemaServer.java
@@ -3,11 +3,10 @@ package com.github.skreileder.exampleTestResourceQuarkusMainLauncher.extension;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import lombok.*;
 import org.eclipse.microprofile.config.ConfigProvider;
 
 import java.io.IOException;
-import java.net.URL;
+import java.net.*;
 import java.nio.file.*;
 import java.util.*;
 
@@ -25,40 +24,48 @@ public class SchemaServer implements QuarkusTestResourceLifecycleManager
   private WireMockServer mockServer;
 
   @Override
-  @SneakyThrows
   public Map<String, String> start()
   {
-    mockServer = new WireMockServer(options().port(33333));
-    mockServer.start();
+    try
+    {
+      mockServer = new WireMockServer(options().port(33333));
+      mockServer.start();
 
-    Path schemaFolder = Path.of(getFilePath("schemas"));
-
-    Files.walk(schemaFolder)
-        .filter(pPath -> !pPath.toFile().isDirectory())
-        .forEach(pFile -> {
-          String path = "/" + pFile.getParent().getFileName() + "/" + pFile.getFileName();
-
-          try
-          {
-            String fileContent = Files.readString(pFile);
-            fileContent = fileContent.replace("<serverUrl>", mockServer.baseUrl());
-
-            WireMock.create().port(33333).build().register(
-                get(urlEqualTo(path))
-                    .willReturn(aResponse()
-                                    .withHeader("Content-Type", "application/json")
-                                    .withBody(
-                                        fileContent
-                                    )));
-          }
-          catch (IOException pE)
-          {
-            throw new RuntimeException(pE);
-          }
-        });
+      Path schemaFolder = null;
+      schemaFolder = Path.of(getFilePath("schemas"));
 
 
-    return Collections.singletonMap(SCHEMA_CATALOG_CONFIG_PARAM, mockServer.baseUrl() + "/schemas/schemas.json");
+      Files.walk(schemaFolder)
+          .filter(pPath -> !pPath.toFile().isDirectory())
+          .forEach(pFile -> {
+            String path = "/" + pFile.getParent().getFileName() + "/" + pFile.getFileName();
+
+            try
+            {
+              String fileContent = Files.readString(pFile);
+              fileContent = fileContent.replace("<serverUrl>", mockServer.baseUrl());
+
+              WireMock.create().port(33333).build().register(
+                  get(urlEqualTo(path))
+                      .willReturn(aResponse()
+                                      .withHeader("Content-Type", "application/json")
+                                      .withBody(
+                                          fileContent
+                                      )));
+            }
+            catch (IOException pE)
+            {
+              throw new RuntimeException(pE);
+            }
+          });
+
+
+      return Collections.singletonMap(SCHEMA_CATALOG_CONFIG_PARAM, mockServer.baseUrl() + "/schemas/schemas.json");
+    }
+    catch (URISyntaxException | IOException pE)
+    {
+      throw new RuntimeException(pE);
+    }
   }
 
   @Override
@@ -83,9 +90,7 @@ public class SchemaServer implements QuarkusTestResourceLifecycleManager
    * @param pFileName the name of the Indexfile
    * @return the path
    */
-  @SneakyThrows
-  @NonNull
-  public static String getFilePath(@NonNull String pFileName)
+  public static String getFilePath(String pFileName) throws URISyntaxException
   {
     ClassLoader classLoader = SchemaServer.class.getClassLoader();
     URL indexFileURL = classLoader.getResource("com/github/skreileder/exampleTestResourceQuarkusMainLauncher/" + pFileName);
